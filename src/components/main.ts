bce5170da855332bba599e6928964c575be6da31
juan martin redondo canessa <arquitectomartinredondo@gmail.com>
2026-01-02 14:36:27 +0100

    update app to working status

--- src/components/main.ts
+++ src/components/main.ts
@@ -4,10 +4,10 @@ import { setupCounter } from './counter.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="../assets/vite.svg" class="logo" alt="Vite logo" />
+      <img src="logo.png" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="../assets/typescript.svg" class="logo vanilla" alt="TypeScript logo" />
+      <img src="typescript.svg" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    // setupFiles: './src/test/setup.js',
-    // reporters: ['json'],
-    // outputFile: 'test.json',
     coverage: {
-      // enabled: true,
       provider: 'v8',
-      // reporter: ['text', 'html', 'lcov'],
-      // failUnderThreshold: true,
-      // reportsDirectory: './coverage',
-      // thresholds: {
-      //   lines: 80,
-      //   functions: 80,
-      //   branches: 75,
-      //   statements: 80
-      // },
-      // exclude: [
-      //   'node_modules/',
-      //   'vite.config.ts',
-      //   'vitest.config.ts',
-      //   'src/**/*.d.ts',
-      //   'src/**/__mocks__/**',
-      //   'src/**/stories/**',
-      //   'src/test/**',
-      //   'src/components/**.test.jsx'
-
-      // ],
-      // include: [
-      //   'src/components/**.jsx'
-      // ]
     },
-    testTimeout: 30000,
   },
 });
+
